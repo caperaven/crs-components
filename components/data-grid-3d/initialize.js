0e6bfd23a104cbdb9bce75ac3dbd13013483a822
caperaven <caperaven@gmail.com>
2021-02-26 23:56:55 +0200

    resize columns animation

--- components/data-grid-3d/initialize.js
+++ components/data-grid-3d/initialize.js
@@ -9,7 +9,7 @@ export async function initialize(parent, dropCallback) {
     await createCanvas(parent);
     await enableGrouping(parent);
     await enableMoveElements(parent.querySelector(".grid-columns"), ".column-header", [".column-header", ".grid-grouping", ".grid-columns"], dropCallback);
-    await enableMoveElements(parent.querySelector(".grid-grouping"), ".column-header", [".column-header", ".grid-grouping"], dropCallback);
+    await enableMoveElements(parent.querySelector(".grid-grouping"), ".column-header-group", [".column-header-group", ".grid-grouping"], dropCallback);
     await enableColumnResize(parent, parent.minColumnWidth);
 }
 
