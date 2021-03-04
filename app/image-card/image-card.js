c5114a0b27376217ea64a6fb9acd567708b32a6f
caperaven <caperaven@gmail.com>
2021-03-04 21:14:34 +0200

    3d grid scrollbox and positioning

--- app/image-card/image-card.js
+++ app/image-card/image-card.js
@@ -4,4 +4,8 @@ export default class ImageCard extends crsbinding.classes.ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
     }
+
+    async newSrc() {
+        this.element.querySelector("image-card").setAttribute("src", "https://i.pinimg.com/originals/62/f5/99/62f5997d012d5decc9dbc8ba0844293a.jpg");
+    }
 }
--- components/image-card/image-card.js
+++ components/image-card/image-card.js
@@ -1,11 +1,26 @@
 class ImageCard extends HTMLElement {
 
+    get src() {
+        return this._src;
+    }
+
+    set src(newValue) {
+        this._src = newValue;
+        if (this.image != null) {
+            this.image.setAttribute("src", newValue);
+        }
+    }
+
     static get observedAttributes() { return ["src"]; }
 
     async connectedCallback() {
         this._mouseDownHandler = this._mouseDown.bind(this);
         this.addEventListener("mousedown", this._mouseDownHandler);
         this.innerHTML = await fetch(import.meta.url.replace(".js", ".html")).then(result => result.text());
+        requestAnimationFrame(() => {
+            this.image = this.querySelector("img");
+            this.image.setAttribute("src", this.src);
+        })
     }
 
     async disconnectedCallback() {
@@ -18,7 +33,7 @@ class ImageCard extends HTMLElement {
     }
 
     async attributeChangedCallback(name, oldValue, newValue) {
-        console.log(name);
+        this[name] = newValue;
     }
 }
 
