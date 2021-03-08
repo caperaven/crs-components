2a9d096aab94df8f46336a4416e2807c9c4f8d0b
caperaven <caperaven@gmail.com>
2021-03-08 13:01:07 +0200

    monaco

--- app/monaco/monaco.js
+++ app/monaco/monaco.js
@@ -1,4 +1,21 @@
 import "./../../components/monaco-editor/monaco-editor.js";
 
 export default class Monaco extends crsbinding.classes.ViewBase {
+    async connectedCallback() {
+        await super.connectedCallback();
+        this.editor = this.element.querySelector("crs-monaco-editor");
+        this.editor.addEventListener("ready", async () => this.showNormal());
+    }
+
+    async showNormal() {
+        this.editor.value = await fetch(import.meta.url).then(result => result.text());
+    }
+
+    async showDifference() {
+        await this.editor.compare("Hello World", "Hello there World", "text/plain");
+    }
+
+    async showDifference2() {
+        await this.editor.compare("function a() {}", "function b() {\n\tconst result = 2;\n}", "javascript");
+    }
 }
--- components/monaco-editor/monaco-editor.js
+++ components/monaco-editor/monaco-editor.js
@@ -7,6 +7,10 @@ class MonacoEditor extends HTMLElement {
         this._editor = newValue;
     }
 
+    get monaco() {
+        return this._monaco;
+    }
+
     get showMiniMap() {
         return this._showMiniMap || this.getAttribute("show-minimap") || true;
     }
@@ -30,6 +34,11 @@ class MonacoEditor extends HTMLElement {
         return this.editor.getValue();
     }
 
+    set value(newValue) {
+        this._value = newValue;
+        this._update();
+    }
+
     get theme() {
         if (this._theme == null) {
             this._theme = this.getAttribute("theme");
@@ -48,10 +57,14 @@ class MonacoEditor extends HTMLElement {
         this.innerHTML = await fetch(import.meta.url.replace(".js", ".html")).then(result => result.text());
         await this._addRequireJS();
         await this._initEditor();
+
+        this.dispatchEvent(new CustomEvent("ready"));
     }
 
     async disconnectedCallback() {
-        this.editor = null;
+        if (this._editor != null) this._editor.dispose();
+        this._editor = null;
+        this._monaco = null;
     }
 
     async _addRequireJS() {
@@ -84,8 +97,61 @@ class MonacoEditor extends HTMLElement {
             options.theme = this.theme;
         }
 
-        require(['vs/editor/editor.main'], () => {
-            this.editor = monaco.editor.create(this, options);
+        return new Promise(resolve => {
+            require(['vs/editor/editor.main'], () => {
+                this._monaco = monaco;
+                this._editor = monaco.editor.create(this, options);
+                this._editor.__type = "normal";
+                resolve();
+            });
+        })
+    }
+
+    async _update() {
+        if (this._editor != null && this._editor.__type != "normal") {
+            this._editor.dispose();
+            this._editor = monaco.editor.create(this);
+            this._editor.__type = "normal";
+        }
+
+        const model = this.monaco.editor.createModel(this._value);
+        model.getLanguageIdentifier();
+        this.monaco.editor.setModelLanguage(model, this.language);
+
+        const options = {
+            language: this.language,
+            minimap: {
+                enabled: this.showMiniMap
+            },
+            parameterHints: {
+                enabled: true,
+                cycle: true
+            }
+        };
+
+        if (this.theme != null) {
+            options.theme = this.theme;
+        }
+
+        this.editor.setModel(model);
+        this._editor.updateOptions(options);
+    }
+
+    async compare(origional, modified, language) {
+        if (this.editor != null && this._editor.__type != "difference") {
+            this.editor.dispose();
+            this.editor = monaco.editor.createDiffEditor(this);
+            this._editor.__type = "difference";
+        }
+
+        language = language || this.language;
+
+        const originalModel = this.monaco.editor.createModel(origional, language);
+        const modifiedModel = this.monaco.editor.createModel(modified, language);
+
+        this.editor.setModel({
+            original: originalModel,
+            modified: modifiedModel
         });
     }
 }
