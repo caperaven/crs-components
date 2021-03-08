2bf9717ad294838267d2b78a2b3018915055ef33
caperaven <caperaven@gmail.com>
2021-03-08 13:14:12 +0200

    monaco

--- app/monaco/monaco.js
+++ app/monaco/monaco.js
@@ -18,4 +18,17 @@ export default class Monaco extends crsbinding.classes.ViewBase {
     async jsDifference() {
         await this.editor.compare("function a() {}", "function b() {\n\tconst result = 2;\n}", "javascript");
     }
+
+    async jsonDifference() {
+        await this.editor.compare(
+            JSON.stringify({
+                "test": "value1"
+            }, null, 4),
+            JSON.stringify({
+                "test-1": "value1",
+                "name": "Test"
+            }, null, 4),
+            "json"
+        )
+    }
 }
