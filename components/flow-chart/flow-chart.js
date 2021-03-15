4d271f79cc0101458d23648c707e337b3280c27e
caperaven <caperaven@gmail.com>
2021-03-15 09:24:05 +0200

    added starter components for flowchart and overlap timeline

---
+++ components/flow-chart/flow-chart.js
@@ -0,0 +1,10 @@
+import {OrthographicCanvas} from "../orthographic-canvas/orthographic-canvas.js";
+
+/**
+ * Note: Gerhard, all yours, gg hf
+ */
+class FlowChart extends OrthographicCanvas {
+
+}
+
+customElements.define("flow-chart", FlowChart);
---
+++ components/overlap-timeline/overlap-timeline.js
@@ -0,0 +1,7 @@
+import {OrthographicCanvas} from "../orthographic-canvas/orthographic-canvas.js";
+
+class OverlapTimeline extends OrthographicCanvas {
+
+}
+
+customElements.define("overlap-timeline", OverlapTimeline);
