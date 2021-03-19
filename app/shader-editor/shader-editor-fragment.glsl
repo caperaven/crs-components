1c43c65701b4601389f9df84b026291a09f3e02d
caperaven <caperaven@gmail.com>
2021-03-19 12:38:20 +0200

    removed some redundant screens and libraries

--- app/shader-editor/shader-editor-fragment.glsl
+++
@@ -1,5 +0,0 @@
-varying vec2 uva;
-
-void main() {
-    gl_FragColor = vec4(uva.x, 0.0, 1.0, 1.0);
-}
--- app/shader-editor/shader-editor-vertex.glsl
+++
@@ -1,6 +0,0 @@
-varying vec2 uva;
-
-void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    uva = uv;
-}
