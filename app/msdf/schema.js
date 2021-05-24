387691d169263b2f9168a14f9d2c562b376980a4
Kieran-Reilly <kieranpreilly@gmail.com>
2021-05-24 08:08:57 +0200

    Updating msdf schema
    Modifying the msdf schema to render the msdf font only, such that I can start testing rendering techniques

--- app/msdf/schema.js
+++ app/msdf/schema.js
@@ -1,33 +1,83 @@
 export const schema = {
-    colors: [
+    textures: [
         {
-            id: "text",
-            color: "#ff0090"
+            id: "font",
+            texture: "/fonts/open-sans/OpenSans-Regular.png"
         }
     ],
-    fonts: [
+    materials: [
         {
-            id: "Open-Sans",
-            font: "/fonts/open-sans/OpenSans-Regular.json",
-            fragment: "/shaders/msdf.frag"
+            id: "blue",
+            type: "RawShaderMaterial",
+            args: {
+                transparent: true,
+                fragmentShader: "/shaders/msdf.frag",
+                uniforms: {
+                    map: {type: "t", value: "font"},
+                    fill: {value: "#ff0090"},
+                    stroke: {value: "#000000"},
+                    strokeWidth: {value: 2},
+                    distanceFactor: {value: 3}
+                }
+            }
         }
     ],
     context: {
-        type: "orthographic",
+        type: "perspective",
         args: {
-            background: "#e8e8e8"
+            background: "#e8e8e8",
+            position: {
+                z: 5
+            }
         }
     },
     scene: {
+        id: 1,
+        title: "MSDF",
         elements: [
             {
-                element: "Text",
-                font: "Open-Sans",
-                text: "Hello World",
-                color: "text",
-                size: 42
+                id: "blue-rect",
+                element: "PlaneGeometry",
+                material: "blue",
+                args: {
+                    transform: {
+                        position: {x: 1, y: 1, z: 0.1},
+                        scale: {x: 2, y: 1}
+                    }
+                }
             }
         ]
     }
 }
+
+// colors: [
+//     {
+//         id: "text",
+//         color: "#ff0090"
+//     }
+// ],
+//     fonts: [
+//     {
+//         id: "Open-Sans",
+//         font: "/fonts/open-sans/OpenSans-Regular.json",
+//         fragment: "/shaders/msdf.frag"
+//     }
+// ],
+//     context: {
+//     type: "orthographic",
+//         args: {
+//         background: "#e8e8e8"
+//     }
+// },
+// scene: {
+//     elements: [
+//         {
+//             element: "Text",
+//             font: "Open-Sans",
+//             text: "Hello World",
+//             color: "text",
+//             size: 42
+//         }
+//     ]
+// }
 
