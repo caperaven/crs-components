0ce4f7eec7044e6a5c2e757a666b7be2fb6fb23a
caperaven <caperaven@gmail.com>
2021-05-24 14:43:32 +0200

    update documenation and refactor drag controls

--- third-party/three/external/controls/DragControls.js
+++ third-party/three/external/controls/DragControls.js
@@ -10,10 +10,6 @@ export class DragControls extends EventDispatcher {
 		this._onPointerMoveHandler = this._onPointerMove.bind(this);
 		this._onPointerDownHandler = this._onPointerDown.bind(this);
 		this._onPointerCancelHandler = this._onPointerCancel.bind(this);
-		this._onPointerCancelHandler = this._onPointerCancel.bind(this);
-		this._onTouchMoveHandler = this._onTouchMove.bind(this);
-		this._onTouchStartHandler = this._onTouchStart.bind(this);
-		this._onTouchEndHandler = this._onTouchEnd.bind(this);
 
 		this.initialize().then(() =>
 			requestAnimationFrame(() => {
@@ -69,21 +65,14 @@ export class DragControls extends EventDispatcher {
 		this._onPointerMoveHandler = null;
 		this._onPointerDownHandler = null;
 		this._onPointerCancelHandler = null;
-		this._onPointerCancelHandler = null;
-		this._onTouchMoveHandler = null;
-		this._onTouchStartHandler = null;
-		this._onTouchEndHandler = null;
 		return null;
 	}
 
 	activate() {
-		this._domElement.addEventListener('pointermove', this._onPointerMoveHandler);
 		this._domElement.addEventListener('pointerdown', this._onPointerDownHandler);
+		this._domElement.addEventListener('pointermove', this._onPointerMoveHandler);
 		this._domElement.addEventListener('pointerup', this._onPointerCancelHandler);
 		this._domElement.addEventListener('pointerleave', this._onPointerCancelHandler);
-		this._domElement.addEventListener('touchmove', this._onTouchMoveHandler);
-		this._domElement.addEventListener('touchstart', this._onTouchStartHandler);
-		this._domElement.addEventListener('touchend', this._onTouchEndHandler);
 	}
 
 	deactivate() {
@@ -91,9 +80,6 @@ export class DragControls extends EventDispatcher {
 		this._domElement.removeEventListener('pointerdown', this._onPointerDownHandler);
 		this._domElement.removeEventListener('pointerup', this._onPointerCancelHandler);
 		this._domElement.removeEventListener('pointerleave', this._onPointerCancelHandler);
-		this._domElement.removeEventListener('touchmove', this._onTouchMoveHandler);
-		this._domElement.removeEventListener('touchstart', this._onTouchStartHandler);
-		this._domElement.removeEventListener('touchend', this._onTouchEndHandler);
 		this._domElement.style.cursor = '';
 	}
 
@@ -117,38 +103,22 @@ export class DragControls extends EventDispatcher {
 
 	_onMouseMove(event) {
 		this._setMouseFromEvent(event);
-
 		this._raycaster.setFromCamera(this._mouse, this._camera);
 
 		if (this._selected && this.scope.enabled ) {
 			if (this._raycaster.ray.intersectPlane(this._plane, this._intersection)) {
 				this._selected.position.copy(this._intersection.sub(this._offset).applyMatrix4(this._inverseMatrix));
 			}
-
 			this.scope.dispatchEvent({type: 'drag', object: this._selected});
-			return;
 		}
 
 		this._intersections.length = 0;
-
 		this._raycaster.setFromCamera(this._mouse, this._camera);
 		this._raycaster.intersectObjects(this._objects, true, this._intersections);
 
 		if (this._intersections.length > 0) {
 			const object = this._intersections[0].object;
-
 			this._plane.setFromNormalAndCoplanarPoint(this._camera.getWorldDirection(this._plane.normal ), this._worldPosition.setFromMatrixPosition(object.matrixWorld));
-
-			if (this._hovered !== object) {
-				this.scope.dispatchEvent( {type: 'hoveron', object: object});
-				this._domElement.style.cursor = 'pointer';
-				this._hovered = object;
-			}
-
-		} else if (this._hovered !== null ) {
-			this.scope.dispatchEvent({type: 'hoveroff', object: this._hovered});
-			this._domElement.style.cursor = 'auto';
-			this._hovered = null;
 		}
 	}
 
@@ -171,8 +141,9 @@ export class DragControls extends EventDispatcher {
 		this._raycaster.setFromCamera(this._mouse, this._camera);
 		this._raycaster.intersectObjects(this._objects, true, this._intersections);
 
-		if (this._intersections.length > 0) {
+		this._selected = null;
 
+		if (this._intersections.length > 0) {
 			this._selected = (this.scope.transformGroup === true ) ? this._objects[0] : this._intersections[0].object;
 
 			if (this._raycaster.ray.intersectPlane(this._plane, this._intersection)) {
@@ -205,55 +176,5 @@ export class DragControls extends EventDispatcher {
 		}
 
 		this._domElement.style.cursor = this._hovered ? 'pointer' : 'auto';
-	}
-
-	_onTouchMove(event) {
-		event.preventDefault();
-		event = event.changedTouches[0];
-
-		this._setMouseFromEvent(event);
-		this._raycaster.setFromCamera(this._mouse, this._camera);
-
-		if (this._selected && this.scope.enabled ) {
-			if (this._raycaster.ray.intersectPlane(this._plane, this._intersection)) {
-				this._selected.position.copy(this._intersection.sub(this._offset).applyMatrix4(this._inverseMatrix));
-			}
-			this.scope.dispatchEvent({type: 'drag', object: this._selected});
-			return;
-		}
-	}
-
-	_onTouchStart(event) {
-		event.preventDefault();
-		event = event.changedTouches[0];
-
-		this._setMouseFromEvent(event);
-		this._intersections.length = 0;
-		this._raycaster.setFromCamera(this._mouse, this._camera);
-		this._raycaster.intersectObjects(this._objects, true, this._intersections);
-
-		if (this._intersections.length > 0) {
-			this._selected = (this.scope.transformGroup === true ) ? this._objects[0] : this._intersections[0].object;
-			this._plane.setFromNormalAndCoplanarPoint(this._camera.getWorldDirection(this._plane.normal ), this._worldPosition.setFromMatrixPosition(this._selected.matrixWorld));
-
-			if (this._raycaster.ray.intersectPlane(this._plane, this._intersection)) {
-				this._inverseMatrix.copy(this._selected.parent.matrixWorld ).invert();
-				this._offset.copy(this._intersection).sub(this._worldPosition.setFromMatrixPosition(this._selected.matrixWorld));
-			}
-
-			this._domElement.style.cursor = 'move';
-			this.scope.dispatchEvent({ type: 'dragstart', object: this._selected});
-		}
-	}
-
-	_onTouchEnd(event) {
-		event.preventDefault();
-
-		if (this._selected ) {
-			this.scope.dispatchEvent({type: 'dragend', object: this._selected});
-			this._selected = null;
-		}
-
-		this._domElement.style.cursor = 'auto';
 	}
 }
