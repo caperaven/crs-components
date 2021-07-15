e237c687ecf3870d35ffc094cfd2ebf2adfb7b40
caperaven <caperaven@gmail.com>
2021-07-15 21:40:52 +0200

    update grid

--- src/components/data-grid/rows/static-rows.js
+++ src/components/data-grid/rows/static-rows.js
@@ -1,6 +1,9 @@
 export default class StaticRows {
     static async enable(grid, data) {
         grid._data = data;
+        grid.settings.pageSize = grid.settings.pageSize || 20;
+        grid.settings.page = 0;
+        await createRows(grid, data);
     }
 
     static async disable(grid) {
@@ -10,8 +13,22 @@ export default class StaticRows {
 }
 
 async function createRows(grid, data) {
+    const fragment = document.createDocumentFragment();
     grid.rows = [];
-    for (let row of data) {
+    const start = grid.settings.page * grid.settings.pageSize;
+    const end = start + grid.settings.pageSize;
+    for (let i = start; i < end; i++) {
+        const row = data[i];
+        await createCells(grid, row, fragment);
+    }
+    grid.bodyElement.appendChild(fragment);
+}
 
+async function createCells(grid, row, fragment) {
+    for (let column of grid._columns) {
+        const element = document.createElement("div");
+        element.setAttribute("role", "gridcell");
+        element.textContent = row[column.field];
+        fragment.appendChild(element);
     }
 }
