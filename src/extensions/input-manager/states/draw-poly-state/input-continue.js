8053ff73f910f692f5c57f6528342e3e9a6df0d6
caperaven <caperaven@gmail.com>
2021-09-02 10:08:42 +0200

    update pen tool to allow curves

--- src/extensions/input-manager/states/draw-poly-state/input-continue.js
+++ src/extensions/input-manager/states/draw-poly-state/input-continue.js
@@ -1,21 +1,19 @@
 import {InputBase, OperationTypes} from "./input-base.js";
 
 export class InputContinue extends InputBase {
-    async pointDown(start, operations, operationType) {
+    async pointDown(start, operations) {
         this._start = start;
         await this._createPoint(start, this.pointColor, "control-point", true);
+        this.p2 = this.shape;
 
         if (operations.length == 0) {
-            await this.moveTo(this._start, operations);
+            return await this.moveTo(this._start, operations);
         }
 
         await this.lineTo(start, operations);
     }
 
     async pointMove(point, operations, operationType) {
-        this.p2 = this.shape;
-        await this.updateLineTo(point, operations);
-
         if (operationType == OperationTypes.LINE) {
             return await this.moveLine(point, operations);
         }
@@ -30,7 +28,7 @@ export class InputContinue extends InputBase {
         delete this._start;
 
         if (this.cp != null) {
-            this._program.canvas.remove(this.cp);
+            this._program.canvas.scene.remove(this.cp);
         }
 
         delete this.cp;
