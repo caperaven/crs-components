fdc4b0b27ab6b3e0d8d494f55b4e95ce8f9f6770
caperaven <caperaven@gmail.com>
2022-05-30 22:37:04 +0200

    add file system component

--- app/file-system/file-system.js
+++ app/file-system/file-system.js
@@ -1,7 +1,12 @@
 import "./../../src/components/file-system/file-system.js"
+import "/node_modules/crs-process-api/action-systems/fs-actions.js"
 
 export default class FileSystem extends crsbinding.classes.ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
+    }
+
+    open() {
+        document.querySelector("file-system").selectFolder();
     }
 }
--- src/components/file-system/file-system.js
+++ src/components/file-system/file-system.js
@@ -7,22 +7,63 @@ export class FileSystem extends HTMLElement {
             this._fileTemplate = this.querySelector("#file-template");
             this._container = this.querySelector(".container");
 
+            this._clickHandler = this._dblClick.bind(this);
+            this.addEventListener("dblclick", this._clickHandler);
+
             this.add("my folder", "folder");
             this.add("my file", "file");
         })
     }
 
     async disconnectedCallback() {
+        this.removeEventListener("dblclick", this._clickHandler);
+        this._clickHandler = null;
+
         this._folderTemplate = null;
         this._fileTemplate = null;
         this._container = null;
+        this._content = null;
     }
 
-    add(title, type) {
+    _dblClick(event) {
+        console.log(event);
+    }
+
+    add(title, type, index) {
         const instance = this[`_${type}Template`].content.cloneNode(true);
         instance.querySelector("div").setAttribute("aria-label", title);
         instance.children[0].querySelector("div").innerText = title;
+        instance.children[0].dataset.index = index;
         this._container.appendChild(instance);
+    }
+
+    async selectFolder() {
+        const content = await crs.call("fs", "open_folder");
+        content.sort((a, b) => {
+            if (a.kind == b.kind) {
+                if (a.name < b.name) {
+                    return -1;
+                }
+            }
+
+            if (a.kind < b.kind) {
+                return -1;
+            }
+
+            return 1;
+        });
+        await this.render(content);
+    }
+
+    async render(content) {
+        this._container.innerHTML = "";
+        this._content = content;
+
+        let index = 0
+        for (const item of content) {
+            this.add(item.name, item.kind == "file" ? "file" : "folder", index);
+            index++;
+        }
     }
 }
 
